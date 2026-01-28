b5fbc3e311331aa618e4e8021ba500b2dd231501
Ayush Kumar <ayush.kumar@w3villa.com>
2026-01-28 16:58:25 +0530

    Added script in package.json and updated env.js

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -3,14 +3,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const env = {
-  port: process.env.PORT,
+  port: process.env.PORT || 5000,
   mongoUri: process.env.MONGO_URI,
   jwtSecret: process.env.JWT_SECRET,
   backendUrl: process.env.BACKEND_URL,
   clientUrl: process.env.CLIENT_URL,
 
   emailHost: process.env.EMAIL_HOST,
-  emailPort: process.env.EMAIL_POST,
+  emailPort: process.env.EMAIL_PORT,
   emailUser: process.env.EMAIL_USER,
   emailPass: process.env.EMAIL_PASS,
   emailFrom: process.env.EMAIL_FROM,
