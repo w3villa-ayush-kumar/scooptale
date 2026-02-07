0ef2a1e2e8582c9201a1bd6923c4bc49be5273b0
Ayush Kumar <ayush.kumar@w3villa.com>
2026-02-07 16:56:23 +0530

    Setted nodemailer again

--- server/src/utils/email.js
+++ server/src/utils/email.js
@@ -1,17 +1,26 @@
-import { Resend } from "resend";
+import nodemailer from "nodemailer";
+import { env } from "../config/env.js";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const transporter = nodemailer.createTransport({
+  host: env.emailHost,
+  port: env.emailPort,
+  secure: false,
+  auth: {
+    user: env.emailUser,
+    pass: env.emailPass,
+  },
+});
 
 export const sendVerificationEmail = async (to, link) => {
-  await resend.emails.send({
-    from: "Scooptale <onboarding@resend.dev>",
+  await transporter.sendMail({
+    from: env.emailFrom,
     to,
     subject: "Verify your Scooptale account",
     html: `
-      <h2>Welcome to Scooptale!</h2>
-      <p>Please verify your email to activate your account.</p>
-      <a href="${link}">Verify Email</a>
-      <p>This link will expire in 24 hours.</p>
-    `,
+        <h2>Welcome to Scooptale!</h2>
+        <p>Please verify your email to activate your account.</p>
+        <a href="${link}" target="_blank">Verify Email</a>
+        <p>This link will expire in 24 hours.</p>
+        `,
   });
 };
